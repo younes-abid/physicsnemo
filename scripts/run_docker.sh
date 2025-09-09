86315c599604b8c95fdd7cc2279697e02453d720
younes.abid <mr.younes.abid@gmail.com>
2025-09-09 14:39:47 +0400

    update run

---
+++ scripts/run_docker.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Run the Docker container in interactive mode with GPU support
+docker run -it --rm \
+    --gpus all \
+    --ipc=host \
+    --ulimit memlock=-1 \
+    --ulimit stack=67108864 \
+    --shm-size=8g \
+    -p 8888:8888 \
+    -p 6006:6006 \
+    -v "$(pwd)/examples:/app/examples" \
+    -v "$(pwd)/physicsnemo:/app/physicsnemo" \
+    -v "$(pwd)/data:/app/data" \
+    -v "$(pwd)/scripts:/app/scripts" \
+    -v "$(pwd)/notebooks:/app/notebooks" \
+    -v "$(pwd)/outputs:/app/outputs" \
+    -v "$(pwd)/outputs/checkpoints_regression:/app/checkpoints_regression" \
+    -v "$(pwd)/outputs/checkpoints_diffusion:/app/checkpoints_diffusion" \
+    -v "$(pwd)/outputs/wandb_regression:/app/wandb_regression" \
+    -v "$(pwd)/outputs/wandb_diffusion:/app/wandb_diffusion" \
+    -v "$(pwd)/outputs/tensorboard:/app/tensorboard" \
+    physiscsnemo:latest
+
+#     -v "/:/app/host" \
