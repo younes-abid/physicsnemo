bd8450feb85bc5ad2e668f6e0a944aaf9d1c979c
younes.abid <mr.younes.abid@gmail.com>
2025-11-27 18:50:26 +0400

    fix command

--- scripts/cmd.sh
+++ scripts/cmd.sh
@@ -17,11 +17,10 @@ ls -1 /mnt/storage/younes.abid/physicsnemo/data/custom_data_2/ERA5_interpolated_
 ls -1 /mnt/storage/younes.abid/physicsnemo/data/custom_data_2/ERA5_WRF_combined_447| wc  -l
 ls -1 /mnt/storage/younes.abid/physicsnemo/data/custom_data_2/ERA5_WRF_combined_concatenated| wc  -l
 
-# scp data 
-scp /Users/younes.abid/Desktop/custom_data_2/WRF_2km.zip younes.abid@10.120.125.144:'/home/younes.abid/git/physicsnemo/data/custom_data_2/ziped'
-scp -r /Users/younes.abid/Desktop/weather_data/custom_data_2/WRF_2km younes.abid@10.120.125.144:'/home/younes.abid/git/physicsnemo/data/custom_data_2/unziped'
-scp -r /Users/younes.abid/Desktop/weather_data/custom_data_2/wrf_with_rainrate_fog younes.abid@10.120.125.144:'/home/younes.abid/git/physicsnemo/data/custom_data_2/unziped'
-rsync -avz --progress --ignore-existing /Users/younes.abid/Desktop/custom_data_2/WRF_2km/ younes.abid@10.120.125.144:/home/younes.abid/git/physicsnemo/data/custom_data_2/unziped/WRF_2km
+# rsync data 
+rsync -avz --progress --ignore-existing /Users/younes.abid/Desktop/custom_data_2/WRF_2km/ younes.abid@10.120.125.144:/home/younes.abid/git/physicsnemo/data/custom_data_2/unziped
+rsync -avz --progress --ignore-existing /Users/younes.abid/Desktop/weather_data/custom_data_2/wrf_with_rainrate_fog younes.abid@10.120.125.144:'/home/younes.abid/git/physicsnemo/data/custom_data_2/unziped'
+
 
 # unzip the data
 unzip '/home/younes.abid/git/physicsnemo/data/custom_data_2/ziped/invariants_2km.nc.zip' -d '/home/younes.abid/git/physicsnemo/data/custom_data_2/unziped'
