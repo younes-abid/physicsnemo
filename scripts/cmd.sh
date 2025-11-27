365a660594c4205a26185dbc9268c6a15d311769
younes.abid <mr.younes.abid@gmail.com>
2025-11-27 18:08:35 +0400

    test access

--- scripts/cmd.sh
+++ scripts/cmd.sh
@@ -1,6 +1,8 @@
 # disk usage
 df -h /mnt/storage/younes.abid
 du -ahx / --exclude=/tmp | sort -rh -T /dev/shm | head -n 20
+du -ahx / --exclude=/tmp | sort -rh -T /System/Volumes | head -n 20
+
 
 # RAM usage
 free -h
@@ -10,13 +12,15 @@ apt-get install -y htop glances
 
 # count files
 ls -1 /mnt/storage/younes.abid/physicsnemo/data/custom_data_2/unziped/WRF_2km | wc  -l
+ls -1 /mnt/storage/younes.abid/physicsnemo/data/custom_data_2/unziped/wrf_with_rainrate_fog | wc  -l
 ls -1 /mnt/storage/younes.abid/physicsnemo/data/custom_data_2/ERA5_interpolated_447| wc  -l
 ls -1 /mnt/storage/younes.abid/physicsnemo/data/custom_data_2/ERA5_WRF_combined_447| wc  -l
 ls -1 /mnt/storage/younes.abid/physicsnemo/data/custom_data_2/ERA5_WRF_combined_concatenated| wc  -l
 
 # scp data 
 scp /Users/younes.abid/Desktop/custom_data_2/WRF_2km.zip younes.abid@10.120.125.144:'/home/younes.abid/git/physicsnemo/data/custom_data_2/ziped'
-scp -r /Users/younes.abid/Desktop/custom_data_2/WRF_2km younes.abid@10.120.125.144:'/home/younes.abid/git/physicsnemo/data/custom_data_2/unziped'
+scp -r /Users/younes.abid/Desktop/weather_data/custom_data_2/WRF_2km younes.abid@10.120.125.144:'/home/younes.abid/git/physicsnemo/data/custom_data_2/unziped'
+scp -r /Users/younes.abid/Desktop/weather_data/custom_data_2/wrf_with_rainrate_fog younes.abid@10.120.125.144:'/home/younes.abid/git/physicsnemo/data/custom_data_2/unziped'
 rsync -avz --progress --ignore-existing /Users/younes.abid/Desktop/custom_data_2/WRF_2km/ younes.abid@10.120.125.144:/home/younes.abid/git/physicsnemo/data/custom_data_2/unziped/WRF_2km
 
 # unzip the data
